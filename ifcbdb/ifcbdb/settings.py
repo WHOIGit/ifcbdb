f83b4f11294c8e5788d7b9e760ff645b43b8a509
Joe Futrelle <joefutrelle@gmail.com>
2019-04-03 13:06:37 -0400

    django now backed by postgres

--- ifcbdb/ifcbdb/settings.py
+++ ifcbdb/ifcbdb/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ifcbdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ifcb',
+        'USER': 'ifcb',
+        'PASSWORD': 'ifcb',
+        'HOST': 'postgres',  # <-- IMPORTANT: same name as docker-compose service!
+        'PORT': '5432',
     }
 }
 
