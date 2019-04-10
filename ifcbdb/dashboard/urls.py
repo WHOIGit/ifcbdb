11f71b093b3422b0fdb2137a225c2144b2eaadab
Joe Futrelle <joefutrelle@gmail.com>
2019-04-10 12:09:17 -0400

    converted tabs to spaces

--- ifcbdb/dashboard/urls.py
+++ ifcbdb/dashboard/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-	path('', views.index),
+    path('', views.index),
 ]
--- ifcbdb/dashboard/views.py
+++ ifcbdb/dashboard/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return render(request, 'index.html', {
+    return render(request, 'index.html', {
 
-	})
+    })
