d44e1a0c6933a91e5a24d3ec27af13861f7c8d35
MattCharron32 <MCharron@Hcfm.com>
2019-04-12 14:38:22 -0400

    Initial commit of page layouts, added Leaflet.

--- ifcbdb/dashboard/urls.py
+++ ifcbdb/dashboard/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('datasets', views.datasets),
+    path('dataset-details', views.dataset_details),
+    path('image-details', views.image_details),
+    path('bin-details', views.bin_details),
 ]
--- ifcbdb/dashboard/views.py
+++ ifcbdb/dashboard/views.py
@@ -6,3 +6,23 @@ def index(request):
 
     })
 
+def datasets(request):
+    return render(request, 'dashboard/datasets.html', {
+
+    })
+
+def dataset_details(request):
+    return render(request, 'dashboard/dataset-details.html', {
+
+    })
+
+def image_details(request):
+    return render(request, 'dashboard/image-details.html', {
+
+    })
+    
+def bin_details(request):
+    return render(request, 'dashboard/bin-details.html', {
+
+    })            
+
