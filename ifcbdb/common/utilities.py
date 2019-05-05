35b26c8a93241f1f1713c1fff29e9bbffcecb24d
Mike Chagnon <mike@kaimika.com>
2019-05-06 00:52:40 +0200

    initial support for changing view size and scale factor on mosaics

--- ifcbdb/common/utilities.py
+++ ifcbdb/common/utilities.py
@@ -11,3 +11,27 @@ def embed_image(image):
     image_data = format_image(image).getvalue()
 
     return base64.b64encode(image_data).decode('ascii')
+
+
+# TODO: Default values should come from common settings
+def parse_scale_factor(value):
+    """
+    Attempts to parse a scale factor from the UI (int) into the decimal required by the mosaic classes
+    """
+    try:
+        return float(value) / 100.0
+    except:
+        return 0.33
+
+
+# TODO: Default values should come from common settings
+def parse_view_size(value):
+    """
+    Attempts to parse a view size from 000x111 format to the tuple required by the mosaic classes. Note that the input
+      string will have width first, whereas the format required by the mosaics has height first
+    """
+    try:
+        dimensions = value.split("x")
+        return (int(dimensions[1]), int(dimensions[0]))
+    except:
+        return (600, 800)
