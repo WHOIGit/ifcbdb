ca420d072d2099f10696dc8ea646a118725ce217
Joe Futrelle <joefutrelle@gmail.com>
2019-07-29 16:08:16 -0400

    remove redundant points from plots, tone down too many images warning

--- ifcbdb/dashboard/views.py
+++ ifcbdb/dashboard/views.py
@@ -508,6 +508,7 @@ def plot_data(request, bin_id):
     ia = bin.images_adc.copy(deep=False)
     ia.columns = schema_names(bin.schema)
     ia['target_number'] = bin.images.keys()
+    ia = ia.drop_duplicates(subset=['roi_x','roi_y']) # reduce redundant data
     if b.has_features():
         features = b.features().fillna(0)
         to_drop = set(bin.images.keys()) - set(features.index)
