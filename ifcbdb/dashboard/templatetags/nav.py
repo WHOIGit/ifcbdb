881a252e85f87f55155d8bc0187d57f8bf1dec66
Mike Chagnon <mike@kaimika.com>
2019-08-27 20:31:28 +0200

    hooked up fields on filter popover, cleaned up code, started work on applying filters; moved chosen library to local/non-cdn reference

--- ifcbdb/dashboard/templatetags/nav.py
+++ ifcbdb/dashboard/templatetags/nav.py
@@ -1,6 +1,6 @@
 from django import template
 
-from dashboard.models import Dataset
+from dashboard.models import Dataset, Instrument, Tag
 
 register = template.Library()
 
@@ -20,4 +20,28 @@ def dataset_nav():
 
     return {
         "datasets": datasets,
+    }
+
+
+@register.inclusion_tag("dashboard/_timeline-filters.html", takes_context=True)
+def timeline_filters(context):
+    dataset = context["request"].GET.get("dataset")
+    instrument = context["request"].GET.get("instrument")
+    tags = context["request"].GET.get("tags")
+    if tags and tags != "":
+        tags = [tag.strip().lower() for tag in tags.split(",")]
+    else:
+        tags = []
+
+    datasets_options = Dataset.objects.all()
+    instruments_options = Instrument.objects.all()
+    tag_options = Tag.objects.all()
+
+    return {
+        "dataset": dataset,
+        "instrument": instrument,
+        "tags": tags,
+        "datasets_options": datasets_options,
+        "instruments_options": instruments_options,
+        "tag_options": tag_options,
     }
