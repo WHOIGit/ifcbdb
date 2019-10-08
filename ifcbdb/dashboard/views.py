bb5ab1b00c3d1786e39b39bf66f4b6f0a1fa0242
Joe Futrelle <joefutrelle@gmail.com>
2019-10-08 12:53:24 -0400

    fixing bug in date handling for dataset search

--- ifcbdb/dashboard/views.py
+++ ifcbdb/dashboard/views.py
@@ -85,7 +85,10 @@ def search_datasets(request):
         region = None
 
     if end_date:
-        end_date = end_date + timedelta(days=1)
+        end_date = pd.to_datetime(end_date) + pd.Timedelta('1d')
+
+    if start_date:
+        start_date = pd.to_datetime(start_date)
 
     datasets = Dataset.search(start_date, end_date, min_depth, max_depth, region=region)
     datasets = list(datasets.values("name", "title"))
