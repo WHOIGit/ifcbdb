266da1ee4d9b19fa18135734786edac8836c9a25
Joe Futrelle <joefutrelle@gmail.com>
2019-11-14 20:19:00 -0500

    adding user field back to newly created tag

--- ifcbdb/dashboard/models.py
+++ ifcbdb/dashboard/models.py
@@ -679,6 +679,8 @@ class Bin(models.Model):
         tag, created = Tag.objects.get_or_create(name=tag_name)
         # don't add this tag if was already added
         event, created = TagEvent.objects.get_or_create(bin=self, tag=tag)
+        if created and user is not None:
+            tag.user = user
         return event
 
     def delete_tag(self, tag_name):
