dfe8798eaf1a5bc87e526089426c8650849b2a8f
Joe Futrelle <joefutrelle@gmail.com>
2022-07-11 16:53:25 -0400

    optionally limit to a specific dataset

--- ifcbdb/dashboard/management/commands/updatetriggers.py
+++ ifcbdb/dashboard/management/commands/updatetriggers.py
@@ -1,4 +1,4 @@
-from dashboard.models import Bin
+from dashboard.models import Bin, bin_query
 from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
 import csv
@@ -9,9 +9,10 @@ class Command(BaseCommand):
     help = 'change labels'
     def add_arguments(self, parser):
         parser.add_argument('-i','--input', type=str, help='Path to csv of mapping of of bin ID and trigger count')
+        parser.add_argument('-d', '--dataset', type=str, help='Name of dataset to process')
 
-    def get_all_bins(self):   
-        all = Bin.objects.all()
+    def get_all_bins(self, dataset=None):
+        all = bin_query(dataset_name=dataset)
         for bin in all:
             res = Bin.objects.filter(pid=bin.pid).update(n_triggers=bin._get_bin().n_triggers)
             if res == 0:
@@ -34,9 +35,10 @@ class Command(BaseCommand):
 
         # handle arguments
         input_csv = options['input']
+        dataset_name = options.get('dataset')
         # validate arguments
         if not input_csv:
-            self.get_all_bins()
+            self.get_all_bins(dataset=dataset_name)
         else:
             self.parse_input_csv(input_csv)
         print("Done.")
