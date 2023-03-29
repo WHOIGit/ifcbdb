ef531b2971e740f19c68717be636179bfa07081f
shravani-whoi <shravani.nagala@whoi.edu>
2023-03-29 14:37:57 -0400

    requested changes: add bin.adc_path, remove user path input, Error handling,

--- ifcbdb/dashboard/management/commands/updatetriggers.py
+++ ifcbdb/dashboard/management/commands/updatetriggers.py
@@ -18,7 +18,7 @@ class Command(BaseCommand):
         parser.add_argument('-d', '--dataset', type=str, help='Name of dataset to process(Optional)')
         parser.add_argument('-p', '--path', type=str, help='absolute path of ifcb data')
 
-        
+
     def last_line(self, filename):
         d = deque(open(filename), 1)
         try:
@@ -33,12 +33,15 @@ class Command(BaseCommand):
         return re.match(r'^(\d+)', line).group(0)
 
 
-    def get_all_bins(self, dataset=None, ifcb_data_path=None):
+    def get_all_bins(self, dataset=None):
+
         all = bin_query(dataset_name=dataset)
-        
         for bin in all:
             # res = Bin.objects.filter(pid=bin.pid).update(n_triggers=bin._get_bin().n_triggers)
-            updated_trigger = self.n_triggers(self.last_line(ifcb_data_path + "/" +str(bin.pid) + ".adc"))
+            if not os.path.exists(bin.adc_path()):
+                print("Error: Bin, " + bin.pid + " adc file not found! Continuing ...")
+                continue
+            updated_trigger = self.n_triggers(self.last_line(bin.adc_path()))
             res = Bin.objects.filter(pid=bin.pid).update(n_triggers=updated_trigger)
             if res == 0:
                 print("Error: Bin, " + bin.pid + " not updated! Continuing ...")
@@ -61,14 +64,10 @@ class Command(BaseCommand):
         # handle arguments
         input_csv = options['input']
         dataset_name = options.get('dataset')
-        ifcb_data_path = options.get('path')
         
         # validate arguments
         if not input_csv:
-            if ifcb_data_path is None:
-                raise ValueError("Please provide the path of ifcb data directory, when not using input_csv")
-            assert os.path.exists(ifcb_data_path)
-            self.get_all_bins(dataset=dataset_name, ifcb_data_path=ifcb_data_path)
+            self.get_all_bins(dataset=dataset_name)
         else:
             self.parse_input_csv(input_csv)
         print("Done.")
