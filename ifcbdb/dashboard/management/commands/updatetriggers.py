6ac9bdd8c14a013f3f9f0a258ca37ee7a9654aa4
shravani-whoi <shravani.nagala@whoi.edu>
2023-03-31 16:10:47 -0400

    add progressbar, bulk update in chunks

--- ifcbdb/dashboard/management/commands/updatetriggers.py
+++ ifcbdb/dashboard/management/commands/updatetriggers.py
@@ -1,17 +1,21 @@
-from dashboard.models import Bin, bin_query
-from django.core.management.base import BaseCommand, CommandError
-from django.db import transaction
+from collections import deque
 import csv
 import os
-
-from glob import glob
-from collections import deque
 import re
-import sys
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
+from tqdm import tqdm
+from tqdm._utils import _term_move_up
+
+from dashboard.models import Bin, bin_query
 
 class Command(BaseCommand):
 
     help = 'update triggers'
+
+    border = "="*50
+    clear_border = _term_move_up() + "\r" + " "*len(border) + "\r"
 
     def add_arguments(self, parser):
         parser.add_argument('-i','--input', type=str, help='Path to csv of mapping of of bin ID and trigger count')
@@ -30,19 +34,40 @@ class Command(BaseCommand):
             return 0
         return re.match(r'^(\d+)', line).group(0)
 
+    def bulk_update(self, chunk, pbar):
+        objs = []
+        for bin in chunk:
+            if not os.path.exists(bin.adc_path()):
+                pbar.write(self.clear_border + ("Error: Bins, " + str(objs) + " not updated! Continuing ..."))
+                pbar.write(self.border)
+                continue
+            bin.n_triggers = self.n_triggers(self.last_line(bin.adc_path()))
+            objs.append(bin)
+        res = Bin.objects.bulk_update(objs, ['n_triggers'])
+        pbar.update(res)
+        if res == 0:
+            pbar.write(self.clear_border + ("Error: Bins, " + str(objs) + " not updated! Continuing ..."))
+            pbar.write(self.border)
+
 
     def get_all_bins(self, dataset=None):
+        count = bin_query(dataset_name=dataset).count()
+        pbar = tqdm(total=count)
 
-        all = bin_query(dataset_name=dataset)
-        for bin in all:
-            # res = Bin.objects.filter(pid=bin.pid).update(n_triggers=bin._get_bin().n_triggers)
-            if not os.path.exists(bin.adc_path()):
-                print("Error: Bin, " + bin.pid + " adc file not found! Continuing ...")
-                continue
-            updated_trigger = self.n_triggers(self.last_line(bin.adc_path()))
-            res = Bin.objects.filter(pid=bin.pid).update(n_triggers=updated_trigger)
-            if res == 0:
-                print("Error: Bin, " + bin.pid + " not updated! Continuing ...")
+        # Bulk update 1000 bins at a time
+        offset = 0
+        limit = 1000
+        partition = 1000
+
+        while True:
+            chunk = bin_query(dataset_name=dataset).order_by('pid')[offset:limit]
+            chunk_length = len(chunk)
+            if chunk_length < 1:
+                break
+            self.bulk_update(chunk, pbar)
+            offset = limit
+            limit = limit + partition
+
 
     def parse_input_csv(self, input_csv):
         if not os.path.exists(input_csv):
