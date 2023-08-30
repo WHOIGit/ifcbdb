b00f32b14999eb39e8838aab28c89b953531bdce
Joe Futrelle <joefutrelle@gmail.com>
2023-08-30 13:33:31 -0400

    eliminate local_settings.py

--- ifcbdb/ifcbdb/settings.py
+++ ifcbdb/ifcbdb/settings.py
@@ -152,7 +152,17 @@ LOGOUT_REDIRECT_URL = 'secure:login'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
-try:
-    from .local_settings import *
-except ImportError as e:
-    raise ImportError('local settings not found') from e
+# local settings configured externally
+
+IFCB_PASSWORD_KEY = 'ignore'
+
+_HOST = os.getenv('NGINX_HOST', 'localhost')
+_HTTPS_PORT = os.getenv('NGINX_HTTPS_PORT', '443')
+_HTTP_PORT = os.getenv('NGINX_HTTP_PORT', '80')
+
+ALLOWED_HOSTS = [_HOST]
+
+CSRF_TRUSTED_ORIGINS = [f'https://{_HOST}:{_HTTPS_PORT}', f'http://{_HOST}:{_HTTP_PORT}']
+
+DEFAULT_DATASET = os.getenv('DEFAULT_DATASET', '')
+
