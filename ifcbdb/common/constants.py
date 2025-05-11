1a689a79c6cc3413448b5740048a64975b2274f8
Mike Chagnon <mike@kaimika.com>
2025-05-11 08:20:38 -0700

    add manager role

--- ifcbdb/common/constants.py
+++ ifcbdb/common/constants.py
@@ -1,5 +1,9 @@
 from enum import Enum
 
+class RoleValues(Enum):
+    ADMIN = "Admin"
+    USER = "User"
+    MANAGER = "Manager"
 
 # Values for this enum should map to their environment variable names
 class Features(Enum):
