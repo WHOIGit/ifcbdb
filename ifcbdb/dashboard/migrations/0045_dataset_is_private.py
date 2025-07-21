38c112f6c820e24d12baca66cefd72a094af8946
Mike Chagnon <mike@kaimika.com>
2025-07-21 13:37:43 -0700

    renumber migrations

--- ifcbdb/dashboard/migrations/0045_dataset_is_private.py
+++ ifcbdb/dashboard/migrations/0045_dataset_is_private.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dashboard', '0038_remove_datadirectory_unique_path_and_more'),
+        ('dashboard', '0044_teamuser_role'),
     ]
 
     operations = [
